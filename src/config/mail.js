e38282774e72ee06e5182e39bb5fe740527f7279
Augusto Kestering Longo <augusto.kestering@gmail.com>
2020-01-14 23:51:33 -0300

    Add nodemailer

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: '2525',
+  secure: false,
+  auth: {
+    user: 'e0ec6903da335f',
+    pass: '703c1ac0613dbe',
+  },
+  default: {
+    from: 'Sample Node <noreply@gobarber.com>',
+  },
+};
